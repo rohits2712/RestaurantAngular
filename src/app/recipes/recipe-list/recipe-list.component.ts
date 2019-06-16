3e035bb7e3347de9f2b0bf639a97e9738a7ec808
Rohit Sharma <rohitrises@gmail.com>
2019-06-16 07:52:36 +0530

    Image addition and component initialization

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+  // recipes= ['rohit','sharma'];
+  recipes:Recipe[] = [
+    new Recipe("A test recipe", "This is a test", "https://thumbs.dreamstime.com/z/healthy-fruits-vegetables-border-frame-21437423.jpg"),
+    new Recipe("A test recipe", "This is a test", "https://thumbs.dreamstime.com/z/healthy-fruits-vegetables-border-frame-21437423.jpg")
+  ];
+  // MyRecipe = this.recipes[0].name;
+  constructor() {
+    console.log(this.recipes);
+   }
 
   ngOnInit() {
   }
